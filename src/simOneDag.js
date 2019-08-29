9ebd7d790ce981c697dd7f9754b3e247dc7d7f28
fxeq <matt.crowe.fxeq@statefarrm.com>
2019-08-28 19:18:53 -0500

    enabling array inputs

--- src/simOneDag.js
+++ src/simOneDag.js
@@ -1,23 +1,10 @@
 'use strict'
-const dagMap1 = require('./dagMaps').dagMap1
-const dagMap2 = require('./dagMaps').dagMap2
 const dagMap3 = require('./dagMaps').dagMap3
 const spaceStationDag = require('./dagSpaceStation').spaceStationDag
-const asyncOneDagProcessorSetup = require('./asyncOneDagProcessor').setup
+const asyncOneDagProcessor_entry = require('./asyncOneDagProcessor').entry
 
-const initSimFunctions = (targetNode, dagMap, inputData) => {
-  if (!targetNode) { console.log('!! args must be targetNode simMap(optional)'); return }
-  if (!dagMap.has(targetNode)) { console.log('!! node not in dag map'); return }
-  if (!inputData) { console.log('!! input data required'); return }
-
-  const modifiedDagMap = modifyDagMapWithInputData(dagMap, inputData)
-  console.log(`Target node is: ${targetNode}`)
-  asyncOneDagProcessorSetup.entry(targetNode, modifiedDagMap, inputData)
-}
-
-const modifyDagMapWithInputData = (dagMap, inputData) => {
-  let inputMap = new Map(Object.entries(JSON.parse(inputData)))
-  console.log(inputMap)
+const modifyDagMapWithInputData = (dagMap, inputDataObject) => {
+  let inputMap = new Map(Object.entries(inputDataObject))
   for (const dagMapObject of dagMap.values()) {
     for (let [inputMapKey, inputMapValue] of inputMap) {
       if (dagMapObject.args.hasOwnProperty(inputMapKey)) {
@@ -28,16 +15,36 @@ const modifyDagMapWithInputData = (dagMap, inputData) => {
   return dagMap
 }
 
-const main = function ([targetNode, selectedDagMap, inputData]) {
-  var dagMap
-  switch (selectedDagMap) {
-    case 'dagMap1': dagMap = dagMap1; break;
-    case 'dagMap2': dagMap = dagMap2; break;
-    case 'spaceStationDag': dagMap = spaceStationDag; console.log(spaceStationDag); break;
+const selectDagMap = (dagMapName) => {
+  let dagMap = {}
+  switch (dagMapName) {
+    case 'spaceStationDag': dagMap = spaceStationDag; break;
     case 'dagMap3': dagMap = new Map([...dagMap1, ...dagMap2, ...dagMap3]); break;
-    default: console.log('cmd format is: npm run-script sim targetNode dagMap simJson'); return;
+    default: return;
   }
-  initSimFunctions(targetNode, dagMap, inputData)
+  return dagMap
+}
+
+const reportFormatError = () => {
+  console.error('cmd format is: npm run-script sim targetNode dagMap simJson')
+}
+
+const main = function ([targetNode, dagMapName, inputData]) {
+  if (!targetNode || !dagMapName || !inputData) { reportFormatError(); return; }
+  let dagMap = selectDagMap(dagMapName)
+  if (!dagMap) { reportFormatError(); return; }
+  if (!dagMap.has(targetNode)) { console.error('!! target node not in dag map'); return }
+
+  const inputDataObject = JSON.parse(inputData)
+  if (Array.isArray(inputDataObject)) {
+    for (let eachInputDataObject of inputDataObject) {
+      let modifiedDagMap = modifyDagMapWithInputData(dagMap, eachInputDataObject)
+      asyncOneDagProcessor_entry(targetNode, modifiedDagMap, inputData)
+    }
+  } else {
+    const modifiedDagMap = modifyDagMapWithInputData(dagMap, inputDataObject)
+    asyncOneDagProcessor_entry(targetNode, modifiedDagMap, inputData)
+  }
 }
 
 exports.main = main
